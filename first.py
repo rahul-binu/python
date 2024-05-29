48282b9b11e9785c42475e18cb9d3a139942d05f
rahul-binu <just.commit.it@gmail.com>
2024-05-29 19:26:44 +0530

    initial commit

---
+++ first.py
@@ -0,0 +1,16 @@
+if(1 == 2):
+    print ('hai')
+else:
+    print ('hello')
+    
+print ('jfjfj',4)
+
+print ('MY CALCULATER')
+print ('-------------')
+def myfunction ():
+    print ('function print')
+    
+value1 = input ('Enter number')
+print (value1)
+
+myfunction()
